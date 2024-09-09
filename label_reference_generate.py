52dd07b3f5a71b1cf75b79a80692d96a12e6a3ce
mn12you <76604100+mn12you@users.noreply.github.com>
2024-09-09 14:28:22 +0800

    modified:   label_reference_generate.py

--- label_reference_generate.py
+++ label_reference_generate.py
@@ -29,18 +29,15 @@ for record in records:
             print("dir exists")
         else:
             file_path.mkdir()
-        save_path="./"+data_base_dir+"/"+patient_id+"/"+patient_id+"_1.csv"
-        np.savetxt(save_path, record_signal_lead_1, delimiter=",")
-        save_path="./"+data_base_dir+"/"+patient_id+"/"+patient_id+"_2.csv"
-        np.savetxt(save_path, record_signal_lead_2, delimiter=",")
-        try:
-            patient_annotation = wfdb.rdann("./mit/"+patient_id,extension="qrsc")
-        except:
-            patient_annotation = wfdb.rdann("./mit/"+patient_id,extension="qrs")
-        patient_symbol=np.array(patient_annotation.symbol)
-        patient_ann=np.stack((patient_annotation.sample,patient_symbol), axis=1)
-        patient_df=pd.DataFrame(patient_ann, columns=['time', 'label'])
-        save_path="./"+data_base_dir+"/"+patient_id+"/"+patient_id+"_ann.csv"
-        patient_df.to_csv(save_path)
-       
+        patient_data = np.stack((record_signal_lead_1, record_signal_lead_2), axis=1)
+        patient_data_df = pd.DataFrame(patient_data, columns=['ECG1', 'ECG2'])
+        save_path = "./" + data_base_dir + "/" + patient_id + "/" + patient_id + ".csv"
+        patient_data_df.to_csv(save_path)
+
+        patient_annotation = wfdb.rdann("./mit/" + patient_id, extension="atr")
+        clean_annotation = [ann.strip('(') for ann in patient_annotation.aux_note]
+        patient_ann = np.stack((patient_annotation.sample, clean_annotation), axis=1)
+        patient_ann_df = pd.DataFrame(patient_ann, columns=['time', 'label'])
+        save_path = "./" + data_base_dir + "/" + patient_id + "/" + patient_id + "_ann.csv"
+        patient_ann_df.to_csv(save_path)
 
