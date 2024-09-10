e51591b1c4e9be26a13f60b4a935e3ec7d0d20f7
mn12you <76604100+mn12you@users.noreply.github.com>
2024-09-10 21:54:58 +0800

    new file:   Check_data.py 	new file:   ECG_ID/.gitkeep 	new file:   ECG_ID_data/.gitkeep 	new file:   LTAFDB/.gitkeep 	modified:   label_reference_generate.py 	deleted:    test_for_phisonet.py

--- Check_data.py
+++ Check_data.py
@@ -1,37 +1,36 @@
 import os
 from pathlib import Path
 import sys
-data_base_dir="AFDB_data"
+data_base_dir="LTAFDB_data"
 import wfdb
 import pandas as pd 
 import numpy as np
 
 
-SCRIPT_DIR="./AFDB/"
+SCRIPT_DIR="./LTAFDB/"
 data_record=Path(SCRIPT_DIR,'RECORDS')
 df=pd.DataFrame(columns=['ecg_id','patient_id','labels','filename'])
 
-bad_rec=['00735','03665', '04043', '04936', '05091', '06453', '08378', '08405', '08434', '08455']
+bad_rec=[]
 records=[]
 with open(data_record) as f:
     records = f.readlines()
-
 
 for record in records:
     patient_id=str(record[:-1])
     if patient_id  not in bad_rec:
         patient_record = wfdb.rdrecord(SCRIPT_DIR+patient_id)
         record_signal_lead_1=patient_record.p_signal[:,0]
-        record_signal_lead_2=patient_record.p_signal[:,1]
+        # record_signal_lead_2=patient_record.p_signal[:,1]
         path=Path(data_base_dir,patient_id)
         file_path=Path(data_base_dir,patient_id)
         if file_path.exists():
             print("dir exists")
         else:
-            file_path.mkdir()
-        patient_data = np.stack((record_signal_lead_1, record_signal_lead_2), axis=1)
-        patient_data_df = pd.DataFrame(patient_data, columns=['ECG1', 'ECG2'])
-        save_path = "./" + data_base_dir + "/" + patient_id + "/" + patient_id + ".csv"
+            os.makedirs(file_path, exist_ok=True)
+        # patient_data = np.stack((record_signal_lead_1, record_signal_lead_2), axis=1)
+        patient_data_df = pd.DataFrame(record_signal_lead_1, columns=['ECG1'])
+        save_path = "./" + data_base_dir + "/" + patient_id + "/" + patient_id[10:]+ ".csv"
         patient_data_df.to_csv(save_path)
 
         patient_annotation = wfdb.rdann(SCRIPT_DIR + patient_id, extension="atr")
--- test_for_phisonet.py
+++
@@ -1,7 +0,0 @@
-import physionet as pn
-
-# Download a dataset
-pn.download('ptbdb', 'data/ptbdb')
-
-# List all datasets
-pn.list_datasets()
